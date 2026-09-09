b335c2b805b3f8d43f14941cf7cb830f318a09f6
Shaurya Juneja <junejashaurya10@gmail.com>
2026-09-08 22:29:28 -0400

    made two variables lower camel case (didnt realized i used snake case there)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,8 +10,8 @@ import 'package:monopoly_money/game_classes/players.dart';
 class JoinScreen extends StatelessWidget {
   JoinScreen({super.key});
 
-  final TextEditingController name_controller = TextEditingController();
-  final TextEditingController game_key_controller = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController gameKeyController = TextEditingController();
 
   late Game game; 
   
@@ -22,14 +22,14 @@ class JoinScreen extends StatelessWidget {
         children: [
           const Text("Create/join a game by entering your player name and a key. Make sure the playername is all lowercase!"), 
           TextField(
-            controller: name_controller,
+            controller: nameController,
             decoration: const InputDecoration(
               labelText: 'Enter your name',
               border: OutlineInputBorder()
             ),
           ),
           TextField(
-            controller: game_key_controller,
+            controller: gameKeyController,
             decoration: const InputDecoration(
               labelText: 'Enter the game key',
               border: OutlineInputBorder()
@@ -38,16 +38,16 @@ class JoinScreen extends StatelessWidget {
           Row(
             children: [
               ElevatedButton(onPressed: () async {
-                final String gameKey = game_key_controller.text.trim();
-                final String name = name_controller.text.trim();
+                final String gameKey = gameKeyController.text.trim();
+                final String name = nameController.text.trim();
                 game = await gameFromExisting(gameKey, name);
                 game.authenticatePlayer(name: name);
 
                 Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => GameScreen(game: game)));
               }, child: const Text('Join game!')),
               ElevatedButton(onPressed: () async {
-                final String gameKey = game_key_controller.text.trim();
-                Player player = Player(gameKey, name_controller.text.trim(), true);
+                final String gameKey = gameKeyController.text.trim();
+                Player player = Player(gameKey, nameController.text.trim(), true);
                 Log(gameKey, 'init').init();
                 player.create();
                 Bank bank = Bank(gameKey, player); 
